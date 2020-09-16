0f512ece10f4131d8dd2f8a7639d0a58fea71a7d
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-16 15:20:03 +0900

    base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-function App() {
-  return <h1>hello, world</h1>;
-}
+import Header from './components/Header';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+}));
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header title="Sample App" />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <h1>hello, world</h1>
+            </Paper>
+          </Grid>
+        </Container>
+      </main>
+    </div>
+  );
+};
 
 export default App;
